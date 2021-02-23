ec7e80d39c1ee26c123fb3a6375e26e993af4729
GibsonGao <78829659+GibsonGao@users.noreply.github.com>
2021-02-24 12:56:26 +1300

    Update AutoLogin.py

--- AutoLogin.py
+++ AutoLogin.py
@@ -5,7 +5,7 @@ driver = webdriver.Chrome()
 def WebPortal_Login():
     driver.get("https://webportal.bdsc.school.nz/index.php/home")
     driver.find_element_by_id("login-username").send_keys("gibson.gao")
-    driver.find_element_by_id("login-password").send_keys("Gg877584")
+    driver.find_element_by_id("login-password").send_keys("")
     driver.find_element_by_class_name("btn btn-info").click()
 
 WebPortal_Login()
